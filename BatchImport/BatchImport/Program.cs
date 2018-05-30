6e47518e6a6ce645ba46555face134331ee868c6
zhangshuai6765 <zhangshuai6765@126.com>
2018-05-30 10:55:34 +0800

    stash

--- BatchImport/BatchImport/Program.cs
+++ BatchImport/BatchImport/Program.cs
@@ -10,8 +10,9 @@ namespace BatchImport
     /// <summary>
     /// 两种批量导入方式
  	/// master分支提交
-	/// test分支提   
-    /// /// </summary>
+	/// test分支提  
+    /// 创建储藏 
+    ///</summary>
     class Program
     {
         static void Main(string[] args)
